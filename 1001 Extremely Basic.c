9c85d6a17c4fc696aa2cf843b8c7d04959fbd9a0
Maruf <maruf1921@gmail.com>
2022-09-27 10:17:57 +0600

    1

---
+++ 1001 Extremely Basic.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main() {
+    int n1, n2;
+    //printf("Enter two numbers: ");
+    scanf("%d %d", &n1, &n2);
+
+    printf("X = %d",n1+n2);
+    printf("\n");
+
+    return 0;
+}
